6bfe2f7a8c8d2f542de4f73e9f3004f3d3d5ee3c
Shohin387 <badalovsohin47@gmail.co>
2025-03-19 00:26:01 +0800

    небольшие поправки а так же добавление похожих товаров на оснве катигории товара

--- src/app/(main)/product/[productId]/layout.tsx
+++ src/app/(main)/product/[productId]/layout.tsx
@@ -1,8 +1,5 @@
-import { metaDataP } from "@/components/product-page-components/conteiner-info";
-import { Metadata } from "next";
 
 
-export const metadata: Metadata = {...metaDataP}
 
 export default function RootLayout({
 	children,
