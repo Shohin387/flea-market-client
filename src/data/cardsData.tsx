92fbe311c24f08032dd962e31b709b8bfbe32278
Shohin387 <132537361+Shohin387@users.noreply.github.com>
2025-03-23 15:40:33 +0800

    Update cardsData.tsx

--- src/data/cardsData.tsx
+++ src/data/cardsData.tsx
@@ -114,7 +114,33 @@ const card: CardsData[] = [
         sold: false
 
     },
+
     
+    {
+        id: 10,
+        userId: 1,
+        nameProduct: "Брюки",
+        imgSrc: "https://avatars.mds.yandex.net/i?id=c606f20bb2a72b3bbeb2f0a25605f40ba5eab465-8187788-images-thumbs&n=13",
+        discription: "Lorem Text",
+        price: 2000,
+        adress: "Улица Пушкина, Дом Колотушкина",
+        category: 'Одежда',
+        coordinate: {lat: 9, lng: 8},
+        sold: false
+    },
+    {
+        id: 11,
+        userId: 1,
+        nameProduct: "Шотры",
+        imgSrc: "https://avatars.mds.yandex.net/i?id=2df2d2cec8bc169d3c49327efc13f838fad7c54a-8497448-images-thumbs&n=13",
+        discription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ratione praesentium error nisi provident ut, quos earum architecto fugiat debitis nesciunt, ad porro. Perspiciatis, dolores, itaque repellat quibusdam exercitationem doloremque accusantium illo aliquid molestias quos blanditiis recusandae commodi quaerat deserunt! Voluptas fuga, veritatis assumenda quia amet quos voluptatum atque provident.",
+        price: 800,
+        adress: "Улица Пушкина, Дом Колотушкина",
+        category: 'Одежда',
+        coordinate: {lat: 9, lng: 8},
+        sold: false
+
+    },
 ]
 
 
